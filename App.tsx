c942ea5f2d29af2f4fbd7d9dd177ee99179e8e24
emre-ast <emre_ast@hotmail.com>
2023-06-23 14:11:01 +0300

    feat: add icons to navbar

--- App.tsx
+++ App.tsx
@@ -1,28 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Button} from 'react-native';
 import {NavigationContainer, ParamListBase} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {PaperProvider} from 'react-native-paper';
+import {PaperProvider, Text} from 'react-native-paper';
+import AntIcons from 'react-native-vector-icons/AntDesign';
 
-function HomeScreen({
+function OrderScreen({
   navigation,
-}: NativeStackScreenProps<ParamListBase, 'Home'>) {
+}: NativeStackScreenProps<ParamListBase, 'Orders'>) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-
-      <Button
-        title="Go to Products"
-        onPress={() => navigation.navigate('Products')}
-      />
-
-      <Button
-        title="Go to Category"
-        onPress={() => navigation.navigate('Category')}
-      />
+      <Text variant="displayLarge">Cross Platform Project</Text>
     </View>
   );
 }
@@ -46,6 +38,8 @@ function CategoryScreen({
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Category Screen</Text>
 
+      <AntIcons name="home" size={24} color="red" />
+
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
@@ -58,31 +52,29 @@ function App() {
     <PaperProvider>
       <NavigationContainer>
         <Tab.Navigator
-          initialRouteName="Home"
-          shifting
+          initialRouteName="Products"
+          backBehavior="initialRoute"
           labeled
-          sceneAnimationEnabled={false}>
-          <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              tabBarIcon: 'home-account',
-            }}
-          />
-          <Tab.Screen
-            name="Products"
-            component={ProductScreen}
-            options={{
-              tabBarIcon: 'bell-outline',
-            }}
-          />
-          <Tab.Screen
-            name="Categories"
-            component={CategoryScreen}
-            options={{
-              tabBarIcon: 'message-text-outline',
-            }}
-          />
+          sceneAnimationEnabled={false}
+          screenOptions={({route}) => ({
+            // eslint-disable-next-line react/no-unstable-nested-components
+            tabBarIcon: ({color}) => {
+              let iconName;
+
+              if (route.name === 'Orders') {
+                iconName = 'profile';
+              } else if (route.name === 'Products') {
+                iconName = 'shoppingcart';
+              } else {
+                iconName = 'tags';
+              }
+
+              return <AntIcons name={iconName} size={24} color={color} />;
+            },
+          })}>
+          <Tab.Screen name="Products" component={ProductScreen} />
+          <Tab.Screen name="Categories" component={CategoryScreen} />
+          <Tab.Screen name="Orders" component={OrderScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </PaperProvider>
